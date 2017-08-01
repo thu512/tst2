d3e681e0bab65223fcc0c945d7636409109c963e
Changjoo <thu512@naver.com>
2017-08-01 11:29:03 +0900

    First Commit

---
+++ app/src/main/java/com/changjoo/testgit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.changjoo.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //20170801작업시작
+    }
+}
